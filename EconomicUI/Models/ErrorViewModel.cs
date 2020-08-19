3c67cf67d6244aafa0a151258ed1ad23605cd3c4
Ian Gregg <iangregg188@gmail.com>
2020-08-19 11:06:30 -0700

    adding all the basic files.
    Co-authored-by: Ben Russell <benjaminrussell36@gmail.com>
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

---
+++ EconomicUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EconomicMappingUI.solution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
